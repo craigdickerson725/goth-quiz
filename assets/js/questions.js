7372f6ce4042663903ee7b351c557f08cc7f14c7
Craig Dickerson <craigdickerson@hotmail.com>
2024-07-25 13:54:52 +0000

    Add javascript questions file

---
+++ assets/js/questions.js
@@ -0,0 +1,64 @@
+/* jshint esversion: 11 */
+// Array for quiz questions and answers/options
+
+let quizArray = [
+    {
+        id: "0",
+        question: "In which year did the gothic musical subculture begin?",
+        options: ["1980:  the year Bauhaus released the album 'In the Flat Field'", "1979:  the year Joy Division released 'Unknown Pleasures'", "What are you, like a historian or something?", "There was no beginning...the darkness has always been waiting for us"],
+        correct: "There was no beginning...the darkness has always been waiting for us",
+    },
+    {
+        id: "1",
+        question: "Who is the 'first lady of Goth'?",
+        options: ["Siouxsie Sioux", "Theda Bara", "Helena Bonham Carter", "Every goth girl is a 'first lady' to me"],
+        correct: "Every goth girl is a 'first lady' to me",
+    },
+    {
+        id: "2",
+        question: "Who is the most fun goth to watch a film with?",
+        options: ["Peter Murphy", "Elvira, Mistress of the Dark", "Christina Ricci", "Cher"],
+        correct: "Elvira, Mistress of the Dark",
+    },
+    {
+        id: "3",
+        question: "When true goths get an ear ache, what do they do?",
+        options: ["Listen to the latest Taylor Swift single", "Get a home remedy from the Goth Medical Journal", "Pray to Hecate", "Scream into the void until the light and darkness become one"],
+        correct: "Scream into the void until the light and darkness become one",
+    },
+    {
+        id: "4",
+        question: "Which classic goth song contains the line: 'The bats have left the belltower'?",
+        options: ["'Red Light' By Siouxsie Sioux and the Banshees", "Ugh, if you have to ask, you'll never really know", "'One More Try' by Timmy T", "'Stay' by Shakespeare's Sister"],
+        correct: "Ugh, if you have to ask, you'll never really know",
+    },
+    {
+        id: "5",
+        question: "What is the highest selling goth cassette single of all time?",
+        options: ["'Black Monday' by New Order", "'Love Will Tear Us Apart' by Joy Division", "'Bela Legosi's Dead' by Bauhaus", "Umm...what is a cassette single?"],
+        correct: "Umm...what is a cassette single?",
+    }, {
+        id: "6",
+        question: "Why did goth kids in the 1980's never brush their hair?",
+        options: ["As a sign of protest", "Because it drew attention away from the despair in their eyes", "Wait...are you saying they brush their hair now?", "Because they lost their hairbrushes during a pagan ritual"],
+        correct: "Wait...are you saying they brush their hair now?",
+    },
+    {
+        id: "7",
+        question: "What is the essential accessory that all goths wear at some point in their lives?",
+        options: ["Blood", "A leather harness", "Fishnet stockings", "Our own flesh is the accessory"],
+        correct: "Our own flesh is the accessory",
+    },
+    {
+        id: "8",
+        question: "What 'mortal sin' are all goths guilty of?",
+        options: ["Hold up...what are you, a cop?", "Blasphemy", "Envy", "Wishing harm on others"],
+        correct: "Hold up...what are you, a cop?",
+    },
+    {
+        id: "9",
+        question: "What do all goths do to survive difficult times in their lives?",
+        options: ["Take long walks in the park to clear their heads", "Go to the beach to feel the sunlight on their skin", "Go to a comedy club to let the power of laughter heal them", "Put on the Cure's 'Disintegration' album and scream through the tears"],
+        correct: "Put on the Cure's 'Disintegration' album and scream through the tears",
+    },
+];
